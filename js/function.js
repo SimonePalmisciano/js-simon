ff7c13e8b1bc6de94a7c03179cee7531be0d3489
Simone Palmisciano <simonepalmisciano11@gmail.com>
2026-04-07 19:20:50 +0200

    modificata funzione verifica numeri perchè aveva dei problemi e spostate alcune funzioni in script

--- js/function.js
+++ js/function.js
@@ -15,19 +15,20 @@ function getRandom() {
 // e mostra i numeri casuali generati
 function displayCounter() {
 
+    countdown.textContent = countdownValue;
+
     countdownValue--;
 
-    if (countdownValue >= 0) {
-        countdown.textContent = countdownValue;
-        displayNumeriCasuali();
-    } else {
+    if (countdownValue < 0) {
         clearInterval(intervalId);
         countdown.classList.add('d-none');
         listaNumeriDaMemorizzare.classList.add('d-none');
         istruzioniDelGioco.textContent = 'Adesso inserisci i numeri che ricordi!!';
+        formRisposte.classList.remove('d-none');
     }
 
 }
+
 
 // inietta i numeri casuali figli di 'li' all'interno dell'html
 function displayNumeriCasuali() {
@@ -51,12 +52,27 @@ function displayNumeriCasuali() {
 
 // creiamo una funzione che verifichi se i numeri inseriti dall'utente 
 // sono corretti
-function verficaNumeriCorrettiHandler (event) {
+function verficaNumeriCorrettiHandler(event) {
     event.preventDefault();
+    // prendo tutti gli input
+    const inputElements = document.querySelectorAll('#answersForm .form-control');
+    // array dove salvo i valori inseriti
+    const valoriInseriti = [];
 
-    if (arrayNumeriCasuali.includes(formRisposte)) {
-        numeriIndovinati ++;
-
-        messaggio.textContent = `Hai indovinato: ${numeriIndovinati} numeri su 5.`
+    for (let z = 0; z < inputElements.length; z++) {
+        const input = inputElements[z];     // l'input alla posizione z
+        const value = input.value;          // stringa inserita dall'utente
+        valoriInseriti.push(Number(value)); // la trasformo subito in numero
     }
+
+    numeriIndovinati = 0;
+    // 4. Confronto ogni numero inserito con i numeri casuali
+    for (let i = 0; i < valoriInseriti.length; i++) {
+        const numeroUtente = valoriInseriti[i];
+        if (arrayNumeriCasuali.includes(numeroUtente)) {
+            numeriIndovinati++;
+        }
+    }
+
+    messaggio.textContent = `Hai indovinato: ${numeriIndovinati} numeri su 5.`
 }
--- js/script.js
+++ js/script.js
@@ -1,24 +1,25 @@
 'use strict';
 
 const countdown = document.querySelector('#countdown');
-const intervalId = setInterval(displayCounter, 1000);
-
 const istruzioniDelGioco = document.querySelector('#instructions');
-
 const listaNumeriDaMemorizzare = document.querySelector('#numbersList');
-
-const formRisposte = document.querySelector('#answersForm .form-control').textContent;
-
+const formRisposte = document.querySelector('#answersForm');
 const bottoneVerifica = document.querySelector('#btnConferma');
 const messaggio = document.querySelector('#message');
 
 let countdownValue = 5; //cambia il timer una volta finite le prove
 let numeriIndovinati = 0;
-
 let arrayNumeriCasuali = [];
 getRandom();
+displayNumeriCasuali();
+formRisposte.classList.add('d-none');
+
+const intervalId = setInterval(displayCounter, 1000);
+// potrei inserire qui il display numeri casuali e nasconderli
+// dopo quando è finito il countdown
+// dovrei mettere che quando setInterval è disattivato o è fermo
 
 
 console.log(arrayNumeriCasuali);
-console.log(formRisposte);
 
+bottoneVerifica.addEventListener ('click', verficaNumeriCorrettiHandler);
