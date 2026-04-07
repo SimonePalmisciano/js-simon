c3a999656e5dab773fd5af099d5a610307bfd164
Simone Palmisciano <simonepalmisciano11@gmail.com>
2026-04-07 16:39:22 +0200

    modificato un po' il layout della pagina html e dichiarate variabili base

---
+++ js/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const countdownSemplice = document.querySelector('#countdown');
+const istruzioniDelGioco = document.querySelector('#instructions');
+const listaNumeriDaMemorizzare = document.querySelector('#numbersList');
+const formRisposte = document.querySelector('#answersForm');
+const bottoneVerifica = document.querySelector('#btnConferma');
+const alertErrore = document.querySelector('#message');
+
